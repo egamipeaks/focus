2b6144c60f521d43a02e604a5e66258ecbd566e3
Andrew Krzynowek <egamipeaks@gmail.com>
2025-08-27 23:15:14 -0500

    building out app shell

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,12 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/focus', function () {
+    return view('focus', [
+        'title' => 'Focus',
+    ]);
+})->name('focus');
+
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
